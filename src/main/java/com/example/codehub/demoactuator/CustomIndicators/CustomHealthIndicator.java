4d1fc3f74fb830b1076416dac0ac70eb1199db98
George Lalas <glalas@intrasoft-intl.com>
2019-01-17 08:16:49 +0200

    Initial Commit - Actuator Endpoints Configuration

---
+++ src/main/java/com/example/codehub/demoactuator/CustomIndicators/CustomHealthIndicator.java
@@ -0,0 +1,16 @@
+//package com.example.codehub.demoactuator.CustomIndicators;
+//
+//import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+//import org.springframework.boot.actuate.health.Health;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class CustomHealthIndicator extends AbstractHealthIndicator {
+//
+//    @Override
+//    protected void doHealthCheck(Health.Builder builder) throws Exception {
+//        builder.up()
+//                .withDetail("app", "Alive and Kicking")
+//                .withDetail("error", "Nothing! I'm good.");
+//    }
+//}
